549f4871ae08fbe55cb3075a2bc16ba8fc46c1a6
sunghyuk <sunghyuk>
2018-12-03 11:08:03 +0100

    Quora

--- quora.py
+++ quora.py
@@ -1,40 +1,49 @@
+#MAX SIZE IS 135
+
+### WORD EMBEDDING
+import gensim
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 data=pd.read_csv('Quora/train.csv')
-data.shape
+data=data.tail(100)
 
-from sklearn.model_selection import train_test_split
+#DELETE STOPWORDS
+from nltk.corpus import stopwords
+text=np.asarray(data.question_text)
+test=[word for word in text if word not in stopwords.words('english')]
 
-train,test=train_test_split(data,test_size=0.1)
-
-### TEXT ANALYSIS
-text=list(test.question_text)
-
-def maxlen (text):
-    #INPIT : text - list of sentences
-    #OUTPUT : max number of words
-    res = 0
-    for q in text:
-        nw = len(q.split())
-        if nw > res:
-            res = nw
-
-    return res
-
-n=maxlen(text)
-
-c=0
-for q in text :
-    c+=1
-    if(len(q.split()) == n):
-        print(q+'\n')
-        print(c)
+words=[]
+for i in range(100):
+    word_list=text[i].split()
+    filt_word = [word for word in word_list if word not in stopwords.words('english')]
+    words.append(filt_word)
 
 
 
 
-### WORD EMBEDDING
+
+
+
+
+model = gensim.models.KeyedVectors.load_word2vec_format('embedding/GoogleNews-vectors-negative300.bin', binary=True)
+
+
+
+
+
+emb=np.zeros( (100,10,300) )
+
+i,j = 0,0
+for t in data.question_text:
+    words=t.split()
+    words=words[0:9]
+    for w in words:
+        emb[i,j,:]=model.wv[w]
+        j+=1
+    i+=1
+
+
 
 
