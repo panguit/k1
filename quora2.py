869177be91833df0a276c38e74db9510a67123c6
sunghyuk <sunghyuk>
2018-12-05 10:33:13 +0100

    Quora_LSTM 2

--- quora2.py
+++ quora2.py
@@ -50,27 +50,85 @@ Y=np.asarray(data.target)
 del model
 
 
+### SPLIT TRAIN TEST
+def split(embed,Y,test_size=0.3):
+    index = np.random.rand(len(embed)) > test_size
+    embed_train = embed[index]
+    embed_test = embed[~index]
+    Y_train = Y[index]
+    Y_test = Y[~index]
+    return (embed_train,Y_train,embed_test,Y_test)
 
+embed_train,Y_train,embed_test,Y_test = split(embed,Y)
+
+
+# 50:50 FOR TARGET
+def mixing50 (embed_train,Y_train) :
+    index= Y_train==1
+
+    embed_1 = embed_train[index]
+    embed_0 = embed_train[~index]
+    embed_0 = embed_0[0:len(embed_1)]
+
+    Y1=Y_train[index]
+    Y0=Y_train[~index]
+    Y0=Y0[0:len(Y1)]
+
+    emb_samp=np.concatenate( (embed_1,embed_0) )
+    Y_samp = np.concatenate( (Y1,Y0) )
+    return (emb_samp,Y_samp)
+
+x_train,y_train=mixing50(embed,Y)
+
+#shuffling data
+from sklearn.utils import shuffle
+x_train = shuffle(x_train,random_state=42)
+y_train = shuffle(y_train,random_state=42)
+y_train=y_train.reshape( (len(y_train),1))
 
 
 #### MODELLING
 from keras.models import Sequential
-from keras.layers import Dense,LSTM
-from sklearn.preprocessing import normalize
+from keras.layers import Dense,LSTM,CuDNNLSTM,BatchNormalization
+from sklearn.metrics import confusion_matrix
 
-X=normalize(embed)
 mod=Sequential()
-mod.add(LSTM(1, input_shape= embed.shape[1:]))
+#mod.add( BatchNormalization())
+mod.add(LSTM(128, input_shape= x_train.shape[1:],return_sequences=  True,activation='relu'))
+mod.add( BatchNormalization())
+mod.add(LSTM(32,activation='relu',return_sequences=True))
+mod.add(LSTM(32,activation='relu'))
+mod.add(Dense(1,activation='sigmoid'))
 
-mod.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+mod.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
 #mod.fit(embed, Y)
 
 
 #BALANCE WEIGHT
 
-class_weights = {0: 1,
-                1: 15}
-mod.fit(embed, Y, class_weight=class_weights)
+#class_weights = {0: 1, 1: 15}
+mod.fit(x_train, y_train ,batch_size=10, epochs= 3)
+#mod.fit(embed,Y,class_weight= class_weights)
 
-from sklearn.metrics import confusion_matrix
-confusion_matrix(Y,mod.predict_classes(embed))
+confusion_matrix(y_train,mod.predict_classes(x_train))
+
+x_test = embed[(len(embed)-1000):len(embed)]
+y_test = Y[(len(embed)-1000):len(embed)]
+y_test.mean()
+
+confusion_matrix(y_test,mod.predict_classes(x_test))
+
+#TEST MODEL ON MNIST SET
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(xm,ym),(xt,yt) = mnist.load_data()
+
+mod1=Sequential()
+#mod.add( BatchNormalization())
+mod1.add(LSTM(32, input_shape= xm.shape[1:],return_sequences=  True,activation='relu'))
+mod1.add( BatchNormalization())
+mod1.add(LSTM(32,activation='relu'))
+mod1.add(Dense(10,activation='softmax'))
+mod1.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+mod1.fit(xm, ym )
